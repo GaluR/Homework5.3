6e368a471e58b096ee0db187c4cd6e5bf604c42d
Bartosz Galinski <galinski.bartosz@gmail.com>
2019-07-18 10:11:07 +0200

    Create new object.

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,14 @@
 public class Test {
     public static void main(String[] args) {
-        Point point1 = new Point(0,10);
-        Point point2 = new Point(0,5);
-        Point point3 = new Point(0,-2);
+        Point point1 = new Point(0,2);
+        Point point2 = new Point(0,1);
+        Point point3 = new Point(10,-2);
 
         Section section1 = new Section();
         Triangle triangle = new Triangle();
         System.out.println(point1.is1Quater(point1.coordinateX, point1.coordinateY));
         System.out.println(section1.lenghOfSection(point1, point2));
-        System.out.println(triangle.isTriangle(section1, point1, point2, point3));
+        System.out.println(triangle.isTriangle(point1, point2, point3));
 
     }
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -1,6 +1,8 @@
 public class Triangle {
 
-   public boolean isTriangle(Section section, Point point1, Point point2, Point point3){
+    Section section = new Section();
+
+   public boolean isTriangle( Point point1, Point point2, Point point3){
        return section.lenghOfSection(point1, point2) + section.lenghOfSection(point2, point3)  != section.lenghOfSection(point1, point3) &&
                section.lenghOfSection(point1, point2) - section.lenghOfSection(point2, point3)  != section.lenghOfSection(point1, point3);
 
